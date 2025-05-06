c4cd4c2151581671022678e39b9f46a8bbb407f7
Felipe <fillikz@gmail.com>
2025-05-06 14:08:36 -0300

    feat(env): configure environment variables validation using zod

---
+++ src/env/index.tsx
@@ -0,0 +1,14 @@
+import "dotenv/config";
+
+import z from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.string().default("development"),
+  DATABASE_URL: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) throw new Error("Invalid Environment Variables");
+
+export const env = _env.data;
